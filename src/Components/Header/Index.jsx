b11d2af8286b31fe1b410b3943c9485da6e175e5
lidvegla <92132152+lidvegla@users.noreply.github.com>
2023-01-04 22:14:35 -0500

    Second commit

--- src/Components/Header/Index.jsx
+++ src/Components/Header/Index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "../../Resources/Logo.svg";
 
 const Container = styled.div`
     height: 8vh;
@@ -28,20 +29,34 @@ const IconContainer = styled.div`
     position: absolute;
     top: 50%;
     left: 95%;
-    transform: translate(-50%, -50%);
+    transform: translate(-30%, -30%);
 `;
 
-const Header = () => {
+const ItemTotal = styled.div`
+    height:2.5vh;
+    width:2.5vh;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    border-radius:50%;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    transform: translate(70%, -50%);
+    color:white;
+    background-color:green;
+`;
+
+export const Header = () => {
     return (
         <Container>
-            <ImgContainer>
+            <ImgContainer href="#">
                 <Image src={Logo} />
             </ImgContainer>
             <IconContainer>
-                
+                <box-icon name="cart"></box-icon>
+                <ItemTotal>0</ItemTotal>
             </IconContainer>
         </Container>
     );
 };
-
-export default Header;
---
+++ src/data.js
@@ -0,0 +1,83 @@
+export const categories = [
+    {
+      id: 1,
+      img:"../ResourcesImg/Female_Category.png",
+      title: "Femenino",
+      category: "Femenino",
+    },
+    {
+        id: 2,
+        img:"../ResourcesImg/Male_Category.png",
+        title: "Masculino",
+        category: "Masculino",
+    },
+    {
+        id: 3,
+        img:"../ResourcesImg/Childish_Category.png",
+        title: "Infantil",
+        category: "Infantil",
+    },
+  ];
+
+export const benefits = [
+    {
+      id: 1,
+      img:"../ResourcesImg/Shield.svg",
+      title: "Pago seguro",
+      desc: "Protegemos los datos de tu compra",
+    },
+    {
+      id: 2,
+      img:"../ResourcesImg/Calendar.svg",
+      title: "Envío rapido",
+      desc: "Envío directo a tu vivienda",
+    },
+    {
+      id: 3,
+      img:"../ResourcesImg/CreditCard.svg",
+      title: "Medios de pago",
+      desc: "Recibimos varios medios de pago",
+    },
+    {
+      id: 4,
+      img:"../ResourcesImg/Truck.svg",
+      title: "A todo Colombia",
+      desc: "Hacemos envios a todo el país",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"../ResourcesImg/Childish_1.png",
+      title: "Nike LD Waffle Sacai Black Nylon",
+      price: 50000,
+      category:"Infantil",
+      cantidad:1,
+    },
+    {
+      id:2,
+      img:"../ResourcesImg/Childish_2.png",
+      title: "Nike LD Waffle Sacai Black Nylon",
+      price: 50000,
+      category:"Infantil",
+      cantidad:1,
+    },
+    {
+      id:3,
+      img:"../ResourcesImg/Childish_3.png",
+      title: "Nike LD Waffle Sacai Black Nylon",
+      price: 50000,
+      category:"Infantil",
+      cantidad:1,
+    },
+    {
+      id:4,
+      img:"../ResourcesImg/Childish_4.png",
+      title: "Nike LD Waffle Sacai Black Nylon",
+      price: 50000,
+      category:"Infantil",
+      cantidad:1,
+    }
+  ]
+
