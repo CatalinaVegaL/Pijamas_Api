0a2bc1ff8fe8e388e5e959168f5809f4acc63ddf
lidvegla <92132152+lidvegla@users.noreply.github.com>
2023-01-05 21:53:51 -0500

    Ajuste de estilos del componente cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,53 +1,52 @@
 import React, { useContext } from "react";
-import Card from "../../Resources/Male_1.png";
 import styled from "styled-components";
-import { DataContext } from "../../Context/Dataprovider"
+import { DataContext } from "../../Context/Dataprovider";
 
+//------------------------------------  STYLES  ------------------------------------//
 const Cars = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content:center;
-    align-items:center;
-    font-size: clamp(10px, 2.5vh, 100px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: clamp(10px, 2.5vh, 100px);
 `;
 
 const Car = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content:center;
-    align-items:center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CarContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    height:30vmin;
-    width:98vmin;
-    margin:1vw ;
-    @media (min-width: 767px) {
-    }
+  display: flex;
+  flex-wrap: wrap;
+  height: 30vmin;
+  width: 98vmin;
+  margin: 1vw;
 `;
 
 const Image = styled.img`
-    width: 20%;
-    height:100%;
+  width: 30%;
+  height: 100%;
+  object-fit: contain;
 `;
 
 const TextCar = styled.div`
-    width:55%;
-    height:100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
+  width: 45%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 `;
 
 const IntCar = styled.div`
-    width:25%;
-    height:100%;
-    display: flex;
-    flex-direction: column;
-    align-items:center;
-    justify-content: space-around;
+  width: 25%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 const Title = styled.div``;
@@ -55,92 +54,113 @@ const Title = styled.div``;
 const Price = styled.div``;
 
 const BottonCar = styled.div`
-    display:flex;
-    align-items:center;
-    justify-content:space-around;
-    width:90px;
-    height:30px;
-    border: 1px solid #646464;
-    border-radius:7%;   
-    transform: scale(0.8);
-    @media (min-width: 767px) {
-        transform: scale(1);
-    }
-    `;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 90px;
+  height: 30px;
+  border: 1px solid #646464;
+  border-radius: 7%;
+  transform: scale(0.8);
+  cursor:pointer;
+  @media (min-width: 767px) {
+    transform: scale(1);
+  }
+`;
 
 const Cant = styled.div`
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    background-color:#64646476;
-    border: 1px solid #646464;
-    width:30px;
-    height:30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #64646476;
+  border: 1px solid #646464;
+  width: 30px;
+  height: 30px;
 `;
 
 const RemoveItem = styled.div`
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    width:30px;
-    height:30px;
-    border: 1px solid #646464;
-    border-radius:7%;
-    background-color:#64646476;
-    transform: scale(0.8);
-    @media (min-width: 767px) {
-        transform: scale(1);
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  border: 1px solid #646464;
+  background-color: #64646476;
+  border-radius: 7%;
+  transform: scale(0.8);
+  cursor:pointer;
+  @media (min-width: 767px) {
+    transform: scale(1);
+  }
 `;
 
 const TotalCar = styled.div`
-    font-weight:700;
-    `;
+  font-weight: 700;
+`;
 
 const Button = styled.div`
-    background-color:black;
-    color:white;
-    text-align:center;
-    margin:20px;
-    padding:2%;
-    width:50%;
-    font-weight:600;
+  background-color: black;
+  color: white;
+  text-align: center;
+  margin: 20px;
+  padding: 2%;
+  width: 50%;
+  font-weight: 600;
+  cursor:pointer;
 `;
 
 export const Cart = () => {
-
     const value = useContext(DataContext);
     const [carrito, setCarrito] = value.carrito;
     const [total] = value.total;
 
+    const removeProduct = id => {
+        if(window.confirm("¿Desea eliminar el producto?")) {
+            carrito.forEach((item, index) => {
+                if (item.id === id) {
+                    item.cantidad = 1;
+                    carrito.splice (index, 1)
+                }
+            })
+            setCarrito([...carrito])
+        }   
+    }
+
     return (
         <Cars>
             <Car>
-            {
-					
-					carrito.length === 0 ? <h2 style={{textAlign: "center", fontSize: "clamp(0px, 2.5vh, 100px)"}}>No tiene nada en el carro de compra</h2> :<>
-					{
-					carrito.map((producto) => (                <CarContainer>
-                        <Image src={producto.image} />
-                        <TextCar>
-                            <Title>{producto.title} </Title>
-                            <Price>${producto.price} </Price>
-                        </TextCar>
-                        <IntCar>
-                            <BottonCar>
-                                <box-icon name="chevron-up"></box-icon>
-                                <Cant>{producto.cantidad}</Cant>
-                                <box-icon name="chevron-down"></box-icon>
-                            </BottonCar>
-                            <RemoveItem>
-                                <box-icon name="trash"></box-icon>
-                            </RemoveItem>
-                        </IntCar>
-                    </CarContainer>))
-				};
-					
-					</>
-					}
+                {carrito.length === 0 ? (
+                    <h2
+                        style={{
+                            textAlign: "center",
+                            fontSize: "clamp(0px, 2.5vh, 100px)",
+                        }}
+                    >
+                        No tiene nada en el carro de compra
+                    </h2>
+                ) : (
+                    <>
+                        {carrito.map((product) => (
+                            <CarContainer>
+                                <Image src={product.image} />
+                                <TextCar>
+                                    <Title>{product.title} </Title>
+                                    <Price>${product.price} </Price>
+                                </TextCar>
+                                <IntCar>
+                                    <BottonCar>
+                                        <box-icon name="chevron-up"></box-icon>
+                                        <Cant>{product.cantidad}</Cant>
+                                        <box-icon name="chevron-down"></box-icon>
+                                    </BottonCar>
+                                    <RemoveItem onClick={() => removeProduct(product.id)}>
+                                        <box-icon name="trash"></box-icon>
+                                    </RemoveItem>
+                                </IntCar>
+                            </CarContainer>
+                        ))}
+                    </>
+                )}
 
                 <TotalCar>Total:$1000000</TotalCar>
             </Car>
